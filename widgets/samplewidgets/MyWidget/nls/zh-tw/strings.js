09157cec0a1b2f44b185839cfb31b357be354a6d
wraysu <wraysu@gmail.com>
2021-01-23 11:03:20 +0800

    commit

---
+++ widgets/samplewidgets/MyWidget/nls/zh-tw/strings.js
@@ -0,0 +1,9 @@
+// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+// See http://js.arcgis.com/3.15/esri/copyright.txt and http://www.arcgis.com/apps/webappbuilder/copyright.txt for details.
+
+//>>built
+define({root:{
+    label1: "我的widget.",
+    label2: "隨便你"
+},
+"zh-tw": true    });
