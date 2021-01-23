23877d8bd4ac274f7574f9a20ed8b0965e48c9e6
wraysu <wraysu@gmail.com>
2021-01-23 10:57:43 +0800

    commit

--- widgets/samplewidgets/MyWidget/Widget.js
+++ widgets/samplewidgets/MyWidget/Widget.js
@@ -2,4 +2,7 @@
 // See http://@sbaseurl@/jsapi/jsapi/esri/copyright.txt and http://www.arcgis.com/apps/webappbuilder/copyright.txt for details.
 
 //>>built
-define(["dojo/_base/declare","jimu/BaseWidget"],function(a,b){return a([b],{baseClass:"jimu-widget-mywidget"})});
+define(["dojo/_base/declare", "jimu/BaseWidget"], function (a, b) { return a([b], { startup: function() {
+    this.inherited(arguments);
+    this.mapIdNode.innerHTML = 'map id is:' + this.map.id;
+  },baseClass: "jimu-widget-mywidget" }) });
