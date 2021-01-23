c9090136030b92ba45d48de2bdb191f6b9195b00
wraysu <wraysu@gmail.com>
2021-01-23 09:41:43 +0800

    commit

---
+++ themes/DemoTheme/panels/SimpleBorderPanel/Panel.js
@@ -0,0 +1,4 @@
+// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+// See http://@sbaseurl@/jsapi/jsapi/esri/copyright.txt and http://www.arcgis.com/apps/webappbuilder/copyright.txt for details.
+//>>built
+define(["dojo/_base/declare","jimu/BaseWidgetPanel"],function(a,b){return a([b],{baseClass:"jimu-panel jimu-border-panel"})});
