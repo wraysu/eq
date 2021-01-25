746db9b43e763c3d7cb41627ff0ba6a0f43ad43d
Su, Wen-Ray <wraysu@gmail.com>
2021-01-25 09:25:08 +0800

    commit

--- widgets/chartJS/Widget.js
+++ widgets/chartJS/Widget.js
@@ -47,7 +47,7 @@ define(['dojo/_base/declare',
           .then(res => {
             return res.json();
           }).then(result => {
-            this.eq = result[0].forEach(item=>{
+            this.eq = result[0].Data.forEach(item=>{
               item.ID = item.WGS84_Lon + '_'+ item.WGS84.Lat;
               this.eqID.push(item.WGS84_Lon + '_'+ item.WGS84.Lat)
           });
